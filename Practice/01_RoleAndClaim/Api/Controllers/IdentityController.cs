480bc96db9c816deb8304ed2b950a12cddc1085b
qizl <q@qizl.cn>
2018-09-19 22:51:09 +0800

    -

---
+++ Practice/01_RoleAndClaim/Api/Controllers/IdentityController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Linq;
+
+namespace Api.Controllers
+{
+    [Route("[controller]")]
+    //[Authorize]
+    public class IdentityController : ControllerBase
+    {
+        [Authorize(Roles = "superadmin")]
+        [HttpGet]
+        public IActionResult Get() => new JsonResult(from c in User.Claims select new { c.Type, c.Value });
+
+        [Authorize(Roles = "admin")]
+        [Route("{id}")]
+        [HttpGet]
+        public string Get(int id) => id.ToString();
+    }
+}
